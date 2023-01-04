0c985d4f5a3acf863c21567a0813476869c7302f
Lu-mi-nous <1115932415@qq.com>
2023-01-04 13:16:45 +0800

    +特殊语音

--- voice.py
+++ voice.py
@@ -34,6 +34,7 @@ header=['Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, li
         "Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.7 (KHTML, like Gecko) Ubuntu/11.04 Chromium/16.0.912.77 Chrome/16.0.912.77 Safari/535.7",
         "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:10.0) Gecko/20100101 Firefox/10.0 "]
 res=re.search("语音key=(.*?)\\n",str(res)).group(1)
+res2=input("特殊语音key：")
 id={'任命助理':'001','交谈1':'002','交谈2':'003','交谈3':'004','晋升后交谈1':'005','晋升后交谈2':'006','信赖提升后交谈1':'007','信赖提升后交谈2':'008','信赖提升后交谈3':'009','闲置':'010','干员报到':'011','观看作战记录':'012','精英化晋升1':'013','精英化晋升2':'014','编入队伍':'017','任命队长':'018','行动出发':'019','行动开始':'020','选中干员1':'021','选中干员2':'022','部署1':'023','部署2':'024','作战中1':'025','作战中2':'026','作战中3':'027','作战中4':'028','完成高难行动':'029','3星结束行动':'030','非3星结束行动':'031','行动失败':'032','进驻设施':'033','戳一下':'034','信赖触摸':'036','标题':'037','问候':'042'}
 workdir=os.getcwd()
 operator=re.search('char_(\d+)_(\w+)',res).group(2) if re.search('char_(\d+)_(\w+)',res) else ""
@@ -83,6 +84,42 @@ for key in pbar2:
     else:
         pbar2.set_description(filename.rstrip("wav")+"mp3已存在!")
     sleep(0.05)
+
+if res2:
+    os.mkdir(workdir+"\\"+op+"\\特殊日文语音") if not os.path.exists(workdir+"\\"+op+"\\特殊日文语音") else print("特殊日文语音目录已创建！")
+    pbar=tqdm(id,unit="file")
+    for key in pbar:
+        filename=pinyin+"_skin1"+"_CN_"+id[key]+".wav"
+        if not os.path.exists(workdir+"\\"+op+"\\特殊日文语音\\"+filename.rstrip("wav")+"mp3"):
+            pbar.set_description("正在获取"+filename.rstrip("wav")+"mp3...")
+            response=requests.get("https://static.prts.wiki/voice/"+res2+"/"+"CN_"+id[key]+".wav",headers={'User-Agent': random.choice(header)})
+            with open(workdir+"\\temp\\"+filename,"wb") as f:
+                f.write(response.content)
+            mp3=AudioSegment.from_wav(workdir+"\\temp\\"+filename)
+            mp3.export(workdir+"\\"+op+"\\特殊日文语音\\"+filename.rstrip("wav")+"mp3",format="mp3")
+            os.remove(workdir+"\\temp\\"+filename)
+            #pbar.set_description(filename.rstrip("wav")+"mp3已获取完成！")
+            #sleep(0.2)
+        else:
+            pbar.set_description(filename.rstrip("wav")+"mp3已存在!")
+        sleep(0.05)
+    os.mkdir(workdir+"\\"+op+"\\特殊中文语音") if not os.path.exists(workdir+"\\"+op+"\\特殊中文语音") else print("特殊中文语音目录已创建！")
+    pbar2=tqdm(id,unit="file")
+    for key in pbar2:
+        filename=pinyin+"_skin1"+"_zh_CN_"+id[key]+".wav"
+        if not os.path.exists(workdir+"\\"+op+"\\特殊中文语音\\"+filename.rstrip("wav")+"mp3"):
+            pbar2.set_description("正在获取"+filename.rstrip("wav")+"mp3...")
+            response=requests.get("https://static.prts.wiki/voice_cn/"+res2+"/"+"CN_"+id[key]+".wav",headers={'User-Agent': random.choice(header)})
+            with open(workdir+"\\temp\\"+filename,"wb") as f:
+                f.write(response.content)
+            mp3=AudioSegment.from_wav(workdir+"\\temp\\"+filename)
+            mp3.export(workdir+"\\"+op+"\\特殊中文语音\\"+filename.rstrip("wav")+"mp3",format="mp3")
+            os.remove(workdir+"\\temp\\"+filename)
+            #pbar2.set_description(filename.rstrip("wav")+"mp3已获取完成！")
+            #sleep(0.2)
+        else:
+            pbar2.set_description(filename.rstrip("wav")+"mp3已存在!")
+        sleep(0.05)
 amount=0
 lost=[]
 for key in id:
